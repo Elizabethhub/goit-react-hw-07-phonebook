2027bdf08ddabdf4f9501c66e0b5b5bba15b4b11
Elizabeth <bagiraliza21@gmail.com>
2021-08-11 22:22:34 +0300

    Updates

---
+++ static/js/6.d1c2e1e3.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-03-phonebook"]=this["webpackJsonpgoit-react-hw-03-phonebook"]||[]).push([[6],{89:function(t,e,n){"use strict";n.r(e);n(0);var i=n(1),o={container:{minHeight:"calc(100vh - 50px)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeight:500,fontSize:48,textAlign:"center"}};e.default=function(){return Object(i.jsx)("div",{style:o.container,children:Object(i.jsx)("h1",{children:"Welcome to Simply the Best Phonebook application!"})})}}}]);
+//# sourceMappingURL=6.d1c2e1e3.chunk.js.map
